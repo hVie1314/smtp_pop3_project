a9ebb17f0a8e535e6c09a41150a620afb68b0e52
hVie1314 <88617436+hVie1314@users.noreply.github.com>
2023-12-27 10:18:35 +0700

    Update smtp.py

--- socket_source/smtp.py
+++ socket_source/smtp.py
@@ -13,17 +13,15 @@ def get_file_type(file_path):
     file_type, _ = mimetypes.guess_type(file_path)
     return file_type
 
-def Connect_to_server():
+def Connect_to_server(mail_server,mail_port_smtp):
 
     #Choose mail server and port
-    mail_server = "127.0.0.1"
-    mail_port = 2225
     #---------------------------
 
     # Create socket called clientSocket and establish a TCP connection with mailserver
     
     clientSocket = socket(AF_INET,SOCK_STREAM)
-    clientSocket.connect((mail_server,mail_port))
+    clientSocket.connect((mail_server,mail_port_smtp))
     
     #---------------------------
 
@@ -119,7 +117,8 @@ def SendMail(clientSocket,user_mail,mail_destination,CC_destination_mail,BCC_des
        attachment = MIMEBase('application', {get_file_type(file_item)})
        attachment.set_payload(file.read())
        encoders.encode_base64(attachment)
-       attachment.add_header('Content-Disposition', f'attachment; filename="{file_item}"')
+       filename = os.path.basename(file_item)
+       attachment.add_header('Content-Disposition', f'attachment; filename="{filename}"')
        msg.attach(attachment)     
       
 
@@ -127,58 +126,6 @@ def SendMail(clientSocket,user_mail,mail_destination,CC_destination_mail,BCC_des
    clientSocket.sendall(msg.as_bytes())
    # Message ends with a single period
    clientSocket.send(endmsg.encode("utf-8"))
-
-   print("Welcome to mail Client\n")
-
-
-
-
-
-mail_destination = input("To: ")
-
-CC_destination_mail = input("CC: ")
-
-BCC_destination_mail = input("BCC: ")
-
-
-
-#Process mail
-if(CC_destination_mail != ""):
- CC_destination_mail = CC_destination_mail.split(",")
-
-if(BCC_destination_mail != ""):
- BCC_destination_mail = BCC_destination_mail.split(",")
-
-user_mail = "duonghuutuong@gmail.com"
-
-mail_subject = input("Subject: ")
-
-mail_content = input("Content:\n")
-
-number_file = input("How many file do you want to attachment:")
-
-attached_files = []
-
-
-number_file = range(int(number_file))
-
-for i in number_file:
-    file_name = input("File {}: ".format(i+1))
-    attached_files.append(file_name)
-
-try:
- clientSocket = Connect_to_server()
-except:
-  print("Connecting To Server Failed !")
-  exit()
-
-try:
- SendMail(clientSocket,user_mail,mail_destination,CC_destination_mail,BCC_destination_mail,attached_files,mail_subject,mail_content)
-except:
-  print("Sending Mail Failed !")
-  exit()
-  
-clientSocket.close()
    
 
-
+   print("\n Đã gửi email thành công \n")
